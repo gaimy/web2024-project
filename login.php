397321ae80a922c1c03b510178cf1153922acce7
gaimy <mungmung660@gmail.com>
2024-06-07 20:14:32 +0900

    sturcture1

---
+++ login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/main.css">
+    <title>Flea Market</title>
+</head>
+<body>
+    <header>
+        <?php require "module/header.php";?>
+    </header>
+    <div class="search">
+        <form action="" method="post">
+            <select name="category" id="category">
+                <option value="books">Books</option>
+                <option value="furnitures">Furnitures</option>
+                <option value="electronics">Electronics</option>
+                <option value="etc">Etc.</option>
+            </select>
+            <input type="search" name="keyword" id="squery">
+        </form>
+    </div>
+    <div class="main-wrap">
+        <section>
+            <article class="item-wrap" >
+                <a class="item-link" href="/view?id=0001">
+                    <img src="" alt="">
+                    <p class="item-title">팔아요</p>
+                    <p class="item-price">100원</p>
+                    <p class="item-desc"></p>
+                </a>
+            </article>
+        </section>
+    </div>
+</body>
+</html>
---
+++ module/header.php
@@ -0,0 +1,9 @@
+<a id="logo" href="">
+    <h3>Flea Market</h3>
+</a>
+<div class="menu ralign">
+    <!-- <p class="profile">Hi, <span>User123</span>!</p> -->
+    <!-- <a href="upload" class="btn"><p>Upload</p></a> -->
+    <a href="login.php" class="btn"><p>Login</p></a>
+    <a href="register.php" class="btn"><p>Register</p></a>
+</div>
